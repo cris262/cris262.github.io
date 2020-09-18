1cbb94354879148e2d53cb17b16e76a781fe26fc
cris262 <crissjc@byui.edu>
2020-09-18 12:21:02 -0400

    finally done

--- js/dates.js
+++ js/dates.js
@@ -4,9 +4,6 @@ let options = {
     year: "numeric"
 };
 
-// shows current date
-document.getElementById("currentDate").textContent = new Date().toLocaleDateString("en-US", options);
-
 // shows when document was last modified
 document.getElementById("lastUpdated").textContent = document.lastModified;
 
