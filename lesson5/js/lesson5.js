dfb2352c96d9e14969324ce82e5595a4ab0c5075
cris262 <crissjc@byui.edu>
2020-10-16 18:44:00 -0400

    l5

---
+++ lesson5/js/lesson5.js
@@ -0,0 +1,24 @@
+window.onload = updateBox;
+
+function updateBox(){
+    let date = new Date().getFullYear();
+    let d = new Date();
+    let monthDay = d.getDate();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+    
+    document.getElementById('updated').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()]+ ' '+ date;
+}
+
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function displayPancake () {
+    var dayOfweek = new Date;
+    if (dayOfWeek.getDay() == 5){
+        document.querySelector("#pancake").style.display = "block";
+    }
+}
